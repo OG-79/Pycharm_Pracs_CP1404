5fe46aebc3b1fe659d5d9e178d88434a08aa9b64
OG-79 <owen.ginn@my.jcu.edu.au>
2021-08-25 14:55:09 +1000

    completed password checker, added functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,15 +1,23 @@
+def main():
+    char_length = 10
+    while True:
+        password = get_password(char_length)
+        if len(password) < char_length:
+            continue
+        else:
+            print_star(password)
+            break
 
-char_length = 10
-while True:
+
+def get_password(char_length):
     password = input("Enter a password at least {} characters long: ".format(char_length))
-    if len(password) < char_length:
-        continue
-    else:
-        for i in range(0, len(password)):
-            print("*", end="")
-        break
+    return password
 
 
+def print_star(password):
+    for i in range(0, len(password)):
+        print("*", end="")
 
 
+main()
 
